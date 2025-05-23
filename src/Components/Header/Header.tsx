b3ccb95b17b5ff5e30e429ebfa8dddd829238589
Divyang Vaghela <dkvaghela724@gmail.com>
2025-05-23 18:47:32 +0530

    Made some small changes in header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,29 +1,42 @@
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "./logo.png";
 import ProfileIcon from "./profile-icon.svg";
 import SearchIcon from "./search icon.svg";
 import ShoppingIcon from "./shopping-icon.svg";
+import MenuIcon from "./menu_icon.svg";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div>
       <header>
         <img className="Logo" src={Logo} alt="" />
-        <div className="Links">
-          <div className="link">
-            <a href="">HOME</a>
+        <nav>
+          <div className={`Links ${menuOpen ? "open" : ""}`}>
+            <div className="link">
+              <a href="">HOME</a>
+            </div>
+            <div className="link">
+              <a href="">COLLECTION</a>
+              <hr className="collection" />
+            </div>
+            <div className="link">
+              <a href="">ABOUT</a>
+            </div>
+            <div className="link">
+              <a href="">CONTACT</a>
+            </div>
           </div>
-          <div className="link">
-            <a href="">COLLECTION</a>
-            <hr className="collection"/>
-          </div>
-          <div className="link">
-            <a href="">ABOUT</a>
-          </div>
-          <div className="link">
-            <a href="">CONTACT</a>
-          </div>
-        </div>
+          <button
+            className="hamburger"
+            onClick={() => setMenuOpen((prev) => !prev)}
+            aria-label="Toggle menu"
+          >
+            <img src={MenuIcon} alt="Menu" style={{ width: 32, height: 32 }} />
+          </button>
+        </nav>
         <div className="icons">
           <img src={ProfileIcon} alt="" />
           <img src={SearchIcon} alt="" />
